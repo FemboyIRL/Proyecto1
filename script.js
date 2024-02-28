da771a2f57b074112053ea569f8706606f730925
FemboyIRL <161182270+FemboyIRL@users.noreply.github.com>
2024-02-27 19:28:29 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
 const michiActualElement = document.getElementById("michiActual");
-const API = "https://dog.ceo/api/breeds/image/random"
+const API_URL = `https://api.thecatapi.com/v1/`;
+const API_KEY = "live_dfGNjMQZcx86RTaaIckppVLtStZpwkP9Kfw6Z12H42hymKg9H6Rk8KbXRCpfnAER";
+const spinner = document.getElementById("lds-heart");
 document.getElementById("Saltar").addEventListener("click",nuevoMichi);
+document.getElementById("Like").addEventListener("click",()=> rankingMichis("+"));
+document.getElementById("Dislike").addEventListener("click",()=> rankingMichis("-"));
+const contenedorLikeMichis = document.getElementById("contenedorLikeMichis");
+const contenedorDislikeMichis = document.getElementById("contenedorDislikeMichis");
+
+spinner.classList.add("escondido");
+
+function nuevoMichi()
+{
+  spinner.classList.toggle("escondido", false);
+  michiActualElement.classList.toggle("escondido", true);
+  const url = `${API_URL}images/search`;
+
+  fetch(url,{headers: {
+    'x-api-key': API_KEY
+  }})
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    res = data[0];
+    michiActualElement.src= res.url;
+  });
+  michiActualElement.classList.toggle("escondido", false);
+  spinner.classList.toggle("escondido", true);
+  }
+
+ function rankingMichis(ranking){
+ 	console.log(ranking);
+ 	const nuevaImagen = document.createElement("img");
+ 	if(ranking === "+"){
+ 		contenedorLikeMichis.appendChild(nuevaImagen);
+ 	}else {
+ 		contenedorDislikeMichis.appendChild(nuevaImagen);
+ 	}
+ 	nuevoMichi();
+ }
 
 
-
-async function nuevoMichi(){
-	const res = await fetch(API);
-	const resJson = await res.json();
-	michiActualElement.src = resJson.message;
-}
-
-//Ejecucion
-
-nuevoMichi();
-
-
+  nuevoMichi();

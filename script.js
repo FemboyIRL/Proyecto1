fa7ee76d7e25e216993edd77e62a24549781f70c
FemboyIRL <161182270+FemboyIRL@users.noreply.github.com>
2024-02-28 19:20:05 -0700

    Likeador de gatos v1.0
    Proyecto1 Teminado UwU

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const michiActualElement = document.getElementById("michiActual");
 const API_URL = `https://api.thecatapi.com/v1/`;
 const API_KEY = "live_dfGNjMQZcx86RTaaIckppVLtStZpwkP9Kfw6Z12H42hymKg9H6Rk8KbXRCpfnAER";
-const spinner = document.getElementById("lds-heart");
+const spinner = document.getElementById("spinner");
 document.getElementById("Saltar").addEventListener("click",nuevoMichi);
 document.getElementById("Like").addEventListener("click",()=> rankingMichis("+"));
 document.getElementById("Dislike").addEventListener("click",()=> rankingMichis("-"));
 const contenedorLikeMichis = document.getElementById("contenedorLikeMichis");
 const contenedorDislikeMichis = document.getElementById("contenedorDislikeMichis");
-
 spinner.classList.add("escondido");
+
 
 function nuevoMichi()
 {
-  spinner.classList.toggle("escondido", false);
-  michiActualElement.classList.toggle("escondido", true);
+  spinner.classList.toggle("escondido",false);
+  michiActualElement.classList.toggle("escondido",true);
   const url = `${API_URL}images/search`;
 
   fetch(url,{headers: {
@@ -24,15 +24,16 @@ function nuevoMichi()
   })
   .then((data) => {
     res = data[0];
-    michiActualElement.src= res.url;
+    michiActualElement.src = res.url;
+    michiActualElement.classList.toggle("escondido",false);
+    spinner.classList.toggle("escondido",true);
   });
-  michiActualElement.classList.toggle("escondido", false);
-  spinner.classList.toggle("escondido", true);
   }
 
  function rankingMichis(ranking){
  	console.log(ranking);
  	const nuevaImagen = document.createElement("img");
+  nuevaImagen.src = michiActualElement.src;
  	if(ranking === "+"){
  		contenedorLikeMichis.appendChild(nuevaImagen);
  	}else {
@@ -40,6 +41,7 @@ function nuevoMichi()
  	}
  	nuevoMichi();
  }
+
 
 
   nuevoMichi();
